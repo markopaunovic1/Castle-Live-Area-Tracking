fd6e9b5bd23ce1b6a9ddc1a14087b286befa49ba
Marko Paunovic <marpau970514@hotmail.com>
2023-06-26 13:11:11 +0200

    Initial Commit

---
+++ CarTrackingTest/CarTrackingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTrackingTestApp.swift
+//  CarTrackingTest
+//
+//  Created by Marko Paunovic on 2023-06-26.
+//
+
+import SwiftUI
+
+@main
+struct CarTrackingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
